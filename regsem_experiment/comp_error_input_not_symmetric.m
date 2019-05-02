4824963855ca5e0a4bc7a1e6559be8828a0e216f
jitkomut songsiri <49604531+jitkomut@users.noreply.github.com>
2019-05-02 13:34:39 +0700

    Add files via upload

---
+++ regsem_experiment/comp_error_input_not_symmetric.m
@@ -0,0 +1,48 @@
+% Compute error of true sparsity pattern
+% ind : the linear index of the first pattern
+% indx : the linear index of the second pattern
+% Both can be generated from sub2ind([n n],i,j) if (i,j) are indices of zero
+% no_of_est_para : number of estimated parameters (used for computing error)
+
+%function [error,true_pos,true_neg,false_pos,false_neg] = comp_error(Atrue,Ahat,n,no_of_est_para)
+function [true_pos,true_neg,false_pos,false_neg] = comp_error(Atrue,Ahat)
+
+mysize = size(Atrue);
+nrow = mysize(1);
+ncol = mysize(2);
+
+ind1 = find(Atrue ~=0);                     %index of nonzero entries in Atrue
+ind2 = find(Atrue == 0);                    %index of zero entries in Atrue
+indx = find(Ahat ~=0);
+indz = find(Ahat == 0);
+
+corr_ind1 = intersect(ind1,indx,'rows');    %intersect the true nonzero entries index
+corr_ind2 = intersect(ind2,indz,'rows');    %intersect the true zero intries index
+wrong_ind = setdiff(indx,ind1,'rows');
+miss_ind = setdiff(ind1,indx,'rows');
+
+% correct nonzero entries
+A = zeros(nrow, ncol);
+A(corr_ind1) = 1;
+% spy(A,'s');hold on;
+true_pos = sum(sum(A));
+
+% correct zero entries
+A = zeros(nrow, ncol);
+A(corr_ind2) = 1;
+% spy(A,'s');hold on;
+true_neg = sum(sum(A));
+
+% plot wrongly-identified sparsity
+A = zeros(nrow, ncol);
+A(wrong_ind) = 1;
+% spy(A,'or');
+false_pos = sum(sum(A));
+
+% plot missed sparsity
+A = zeros(nrow, ncol);
+A(miss_ind) = 1;
+% spy(A,'+k');
+false_neg = sum(sum(A));
+
+%error = (false_pos + false_neg)/no_of_est_para;
